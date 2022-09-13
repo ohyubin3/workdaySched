03e658acc619876b2ca5f7eed10f4dd9f9c43957
Sang Oh <baul3oh@gmail.com>
2022-09-13 10:00:34 -0500

    save before class'

--- Develop/script.js
+++ Develop/script.js
@@ -5,47 +5,31 @@ let timeEl = document.querySelector(".whatTime");
 let toDoEl = document.querySelector(".whatTodo");
 let saveButtons = document.querySelectorAll(".saveBtn");
 let textAreas = document.querySelectorAll(".whatTodo");
+let container = document.querySelector(".container");
 
 renderStorage();
-console.log(renderStorage);
 
-for (let i = 0; i < saveButtons.length; i++) {
-  saveButtons[i].addEventListener("click", function (event) {
-    // console.log(event.target);
-    // console.log(textAreas[i].value);
-    localStorage.setItem(`hourTodo${i}`, textAreas[i].value);
-  });
-}
+container.addEventListener("click", function (event) {
+  if (event.target.matches("button")) {
+    localStorage.setItem(
+      `hourTodo${event.target.parentElement.dataset.hour}`,
+      event.target.previousElementSibling.value
+    );
+    console.log(event.target.previousElementSibling.value);
+    // console.log(event.target.parentElement.dataset.hour);
+  }
+});
+
+// for (let i = 0; i < saveButtons.length; i++) {
+//   saveButtons[i].addEventListener("click", function (event) {
+//     // console.log(event.target);
+//     // console.log(textAreas[i].value);
+//   });
+// }
 
 function renderStorage() {
-  for (let i = 0; i < textAreas.length; i++) {
-    localStorage.getItem(`hourTodo${i}`, textAreas[i].value);
+  for (let i = 8; i < 17; i++) {
+    // localStorage.getItem(`whatTodo${i}`);
+    console.log(localStorage.getItem(`hourTodo$`{i}));
   }
 }
-
-// This is what the Save Button will do
-// saveButtons.addEventListener("click", function (event) {
-//   event.preventDefault();
-
-//   let hourTodo = {
-//     Hours: timeEl.textContent,
-//     Todo: toDoEl.value.trim(),
-//   };
-
-//   localStorage.setItem("hourTodo", JSON.stringify(whatTodo));
-//   console.log(whatTodo);
-// });
-
-// var arr = [
-//   ` <button
-//     type="button"
-//     class="btn btn-primary col-xl-2 col-m-1 col-s-2 text-wrap saveBtn"
-//   >
-//     SAVE
-//   </button>`, `<button
-//   type="button"
-//   class="btn btn-primary col-xl-2 col-m-1 col-s-2 text-wrap saveBtn"
-// >
-//   SAVE
-// </button>`
-//
